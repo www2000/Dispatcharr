573655d269acf1332d5bc94c957231af762c3dc2
kappa118 <kappa118@gmail.com>
2025-03-01 18:37:26 -0500

    new dockerfiles for dev and prod containers, updated / added new compose files for standalone, dev, and aio

---
+++ docker/entrypoint.aio.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Check the value of DISPATCHARR_ENV and run the corresponding program
+case "$DISPATCHARR_ENV" in
+    "dev")
+        echo "DISPATCHARR_ENV is set to 'dev'. Running Development Program..."
+        cd /app/frontend && npm install
+        exec /usr/sbin/uwsgi --ini uwsgi.dev.ini
+        ;;
+    "aio")
+        echo "DISPATCHARR_ENV is set to 'aio'. Running All-in-One Program..."
+        exec /usr/sbin/uwsgi --ini uwsgi.aio.ini
+        ;;
+    *)
+        echo "DISPATCHARR_ENV is not set or has an unexpected value. Running standalone..."
+        exec /usr/sbin/uwsgi --ini uwsgi.ini
+        ;;
+esac
