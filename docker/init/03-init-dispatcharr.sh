01f5b99f72d8781a4a8a0c66f71bb9397dec0017
dekzter <kappa118@gmail.com>
2025-03-06 16:33:30 -0500

    conditional manage perms since mac doesn't need it

--- docker/init/03-init-dispatcharr.sh
+++ docker/init/03-init-dispatcharr.sh
@@ -2,7 +2,12 @@
 
 # Required so both uwsgi and nginx (www-data) can use it
 # @TODO: change nginx to run as the same use as uwsgi
-touch /app/uwsgi.sock
-chown -R $PUID:$PGID /app
-chown $PUID:www-data /app/uwsgi.sock
-chmod 777 /app/uwsgi.sock
+
+# NOTE: mac doesn't run as root, so only manage permissions
+# if this script is running as root
+if [ "$(id -u)" = "0" ]; then
+    touch /app/uwsgi.sock
+    chown -R $PUID:$PGID /app
+    chown $PUID:www-data /app/uwsgi.sock
+    chmod 777 /app/uwsgi.sock
+fi
