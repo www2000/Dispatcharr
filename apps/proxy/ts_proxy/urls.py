140937a192044e5224b3614742b6f262612d8242
SergeantPanda <chester.cullen@gmail.com>
2025-03-13 20:54:30 -0500

    Added detailed status for active channels including connected clients.

--- apps/proxy/ts_proxy/urls.py
+++ apps/proxy/ts_proxy/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('stream/<str:channel_id>', views.stream_ts, name='stream'),
     path('initialize/<str:channel_id>', views.initialize_stream, name='initialize'),
     path('change_stream/<str:channel_id>', views.change_stream, name='change_stream'),
+    path('status', views.channel_status, name='channel_status'),
+    path('status/<str:channel_id>', views.channel_status, name='channel_status_detail'),
 ]
