1812c36ffb5fff906362aa1029a2e717d2328577
dekzter <kappa118@gmail.com>
2025-03-17 19:56:15 -0400

    attempting to fix assets path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   // The base URL for the build, adjust this to match your desired path
-  build: {
-    // Make sure assets are placed correctly in the build output
-    assetsDir: 'static/assets',
-  },
-
   plugins: [react()],
 
   server: {
