d7f96005b88156640cc7be24f3ea950e8ae94b12
dekzter <kappa118@gmail.com>
2025-03-27 09:27:45 -0400

    stream preview functionality

--- frontend/src/components/tables/StreamsTable.jsx
+++ frontend/src/components/tables/StreamsTable.jsx
@@ -44,6 +44,8 @@ import {
   IconSquarePlus,
 } from '@tabler/icons-react';
 import { useNavigate } from 'react-router-dom';
+import useSettingsStore from '../../store/settings';
+import useVideoStore from '../../store/useVideoStore';
 
 const StreamsTable = ({}) => {
   const theme = useMantineTheme();
@@ -91,6 +93,10 @@ const StreamsTable = ({}) => {
   const channelSelectionStreams = useChannelsStore(
     (state) => state.channels[state.channelsPageSelection[0]?.id]?.streams
   );
+  const {
+    environment: { env_mode },
+  } = useSettingsStore();
+  const { showVideo } = useVideoStore();
 
   const isMoreActionsOpen = Boolean(moreActionsAnchorEl);
 
@@ -432,6 +438,14 @@ const StreamsTable = ({}) => {
     setPagination(updater);
   };
 
+  function handleWatchStream(streamHash) {
+    let vidUrl = `/proxy/ts/stream/${streamHash}`;
+    if (env_mode == 'dev') {
+      vidUrl = `${window.location.protocol}//${window.location.hostname}:5656${vidUrl}`;
+    }
+    showVideo(vidUrl);
+  }
+
   const table = useMantineReactTable({
     ...TableHelper.defaultProperties,
     columns,
@@ -564,6 +578,11 @@ const StreamsTable = ({}) => {
               disabled={!row.original.is_custom}
             >
               Delete Stream
+            </Menu.Item>
+            <Menu.Item
+              onClick={() => handleWatchStream(row.original.stream_hash)}
+            >
+              Preview Stream
             </Menu.Item>
           </Menu.Dropdown>
         </Menu>
