d418cd7c0680d46cfbfd9524bef093c6763dd302
dekzter <kappa118@gmail.com>
2025-03-27 13:02:19 -0400

    new custom properties fields for m3u and program data

--- apps/epg/models.py
+++ apps/epg/models.py
@@ -44,6 +44,7 @@ class ProgramData(models.Model):
     sub_title = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     tvg_id = models.CharField(max_length=255, null=True, blank=True)
+    custom_properties = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.title} ({self.start_time} - {self.end_time})"
