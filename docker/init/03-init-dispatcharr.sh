7a90cc8ae33bb7c867dbd231ed63e2f7f2f9027f
dekzter <kappa118@gmail.com>
2025-04-07 08:36:11 -0400

    re-adding in removal of frontend artifacts from dockerfile, re-added chown of /app directory

--- docker/init/03-init-dispatcharr.sh
+++ docker/init/03-init-dispatcharr.sh
@@ -16,9 +16,7 @@ sed -i "s/NGINX_PORT/${DISPATCHARR_PORT}/g" /etc/nginx/sites-enabled/default
 if [ "$(id -u)" = "0" ]; then
     # Needs to own ALL of /data except db, we handle that below
     chown -R $PUID:$PGID /data
-
-    chown -R $PUID:$PGID /app/logo_cache
-    chown -R $PUID:$PGID /app/media
+    chown -R $PUID:$PGID /app
 
     # Permissions
     chown -R postgres:postgres /data/db
