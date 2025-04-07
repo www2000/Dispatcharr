f8e5ca2a1c9c39ea438aaf7bff7d187986bec39a
SergeantPanda <chester.cullen@gmail.com>
2025-04-07 17:18:12 -0500

    fix: update tvg_name assignment in generate_m3u function to use channel.name directly

--- apps/output/views.py
+++ apps/output/views.py
@@ -24,7 +24,7 @@ def generate_m3u(request, profile_name=None):
     for channel in channels:
         group_title = channel.channel_group.name if channel.channel_group else "Default"
         tvg_id = channel.channel_number or channel.id
-        tvg_name = channel.tvg_id or channel.name
+        tvg_name = channel.name
         tvg_logo = channel.logo.url if channel.logo else ""
         channel_number = channel.channel_number
 
