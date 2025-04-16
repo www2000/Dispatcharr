1297ca4c260463b2d7579ea9a0fbca56eb1c3f6c
SergeantPanda <chester.cullen@gmail.com>
2025-04-16 09:42:56 -0500

    bump build

--- version.py
+++ version.py
@@ -2,4 +2,4 @@
 Dispatcharr version information.
 """
 __version__ = '0.3.1'  # Follow semantic versioning (MAJOR.MINOR.PATCH)
-__build__ = '0'        # Auto-incremented on builds
+__build__ = '1'        # Auto-incremented on builds
