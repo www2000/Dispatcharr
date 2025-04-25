3dcc4902faea068c9fd529aa2679f85cb90a9244
dekzter <kappa118@gmail.com>
2025-04-25 13:44:54 -0400

    fixed epg cache

--- apps/epg/models.py
+++ apps/epg/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django_celery_beat.models import PeriodicTask
+from django.conf import settings
 
 class EPGSource(models.Model):
     SOURCE_TYPE_CHOICES = [
@@ -28,6 +29,15 @@ class EPGSource(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_cache_file(self):
+        # Decide on file extension
+        file_ext = ".gz" if self.url.lower().endswith('.gz') else ".xml"
+        filename = f"{self.id}{file_ext}"
+
+        # Build full path in MEDIA_ROOT/cached_epg
+        cache_dir = os.path.join(settings.MEDIA_ROOT, "cached_epg")
+        cache = os.path.join(cache_dir, filename)
 
 class EPGData(models.Model):
     # Removed the Channel foreign key. We now just store the original tvg_id
