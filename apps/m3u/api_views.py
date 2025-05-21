1087568de73afaeff172548b14fa807d765f1a45
SergeantPanda <chester.cullen@gmail.com>
2025-05-21 14:22:01 -0500

    Fix bug when using file upload for m3u and not being able to set "Use Default"

--- apps/m3u/api_views.py
+++ apps/m3u/api_views.py
@@ -51,7 +51,15 @@ class M3UAccountViewSet(viewsets.ModelViewSet):
             # Add file_path to the request data so it's available during creation
             request.data._mutable = True  # Allow modification of the request data
             request.data['file_path'] = file_path  # Include the file path if a file was uploaded
-            request.data.pop('server_url')
+            
+            # Handle the user_agent field - convert "null" string to None
+            if 'user_agent' in request.data and request.data['user_agent'] == 'null':
+                request.data['user_agent'] = None
+                
+            # Handle server_url appropriately
+            if 'server_url' in request.data and not request.data['server_url']:
+                request.data.pop('server_url')
+                
             request.data._mutable = False  # Make the request data immutable again
 
         # Now call super().create() to create the instance
@@ -82,16 +90,24 @@ class M3UAccountViewSet(viewsets.ModelViewSet):
             # Add file_path to the request data so it's available during creation
             request.data._mutable = True  # Allow modification of the request data
             request.data['file_path'] = file_path  # Include the file path if a file was uploaded
-            request.data.pop('server_url')
+            
+            # Handle the user_agent field - convert "null" string to None
+            if 'user_agent' in request.data and request.data['user_agent'] == 'null':
+                request.data['user_agent'] = None
+                
+            # Handle server_url appropriately
+            if 'server_url' in request.data and not request.data['server_url']:
+                request.data.pop('server_url')
+                
             request.data._mutable = False  # Make the request data immutable again
 
             if instance.file_path and os.path.exists(instance.file_path):
                 os.remove(instance.file_path)
 
-        # Now call super().create() to create the instance
+        # Now call super().update() to update the instance
         response = super().update(request, *args, **kwargs)
 
-        # After the instance is created, return the response
+        # After the instance is updated, return the response
         return response
 
     def partial_update(self, request, *args, **kwargs):
